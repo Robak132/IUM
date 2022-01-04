6b143261ba34a97055a5de8f808be4ab7f1fffbe
Jakub Robaczewski <kubarob13@gmail.com>
2022-01-04 20:55:27 +0100

    Cleaning

--- client_analiser/main.py
+++ client_analiser/main.py
@@ -1,12 +1,10 @@
-import random
 import uvicorn
+from random import choice
 from fastapi import FastAPI, Response, Request
-from starlette.templating import Jinja2Templates
 
 from client_analiser.models.predict_model import predict_a, predict_b
 
 app = FastAPI()
-template = Jinja2Templates(directory="templates/")
 
 
 @app.get("/")
@@ -15,30 +13,37 @@ async def root():
 
 
 @app.post("/predictA")
-async def root(request: Request, response: Response):
+async def get_prediction_a(request: Request, response: Response):
     input_data = await request.json()
     return {"Result": predict_a(input_data)}
 
 
 @app.post("/predictB")
-async def root(request: Request, response: Response):
+async def get_prediction_b(request: Request, response: Response):
     input_data = await request.json()
     return {"Result": predict_b(input_data)}
 
 
 @app.get('/form')
-def form_post(request: Request, response: Response):
-    return template.TemplateResponse('index.html', {"request": request, "response": response})
-
-
-@app.post("/predict")
-async def root(request: Request, response: Response):
+async def get_prediction_ab(request: Request, response: Response):
     input_data = await request.json()
-
-    if random.choice([True, False]):
+    if choice([True, False]):
         model = predict_a
     else:
         model = predict_b
+    return {"Result": model(input_data)}
+
+
+@app.post("/predict")
+async def get_predictction_a(request: Request, response: Response):
+    input_data = await request.json()
+
+    if choice([True, False]):
+        model = predict_a
+    else:
+        model = predict_b
+
+    print(f"Using model {model}")
 
     return {"Result": model(input_data)}
 
